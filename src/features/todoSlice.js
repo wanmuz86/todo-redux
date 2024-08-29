f22efd2376a549488128f33f3fc3d631c18c5206
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-29 14:34:30 +0800

    complete todoSlice setup

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,17 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const todoSlice = createSlice({
+    // The name of the configuration/ slice
     name:'todos',
+    /// The state is stored
     initialState:{todos:[]},
+
+    // The action/methods called by the components is defined
     reducers: {
         addTodo: (state,action) => {
+            state.todos.push(action.payload)
+           // state.todos = [...state.todos, action.payload]
 
         },
         removeTodo: (state,action) => {
-
+            state.todos = state.todos.filter(val=> val.id !== action.payload)
         }
     }
 });
 
+// You need to export the function individually
 export const {addTodo, removeTodo} = todoSlice.actions
 export default todoSlice.reducer
